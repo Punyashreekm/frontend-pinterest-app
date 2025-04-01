364521ba22b21e9426f5f7f39852e7e0cecfd51d
punya <punya8144@gmail.com>
2025-04-01 20:38:00 +0530

    add : styles for gallery

--- src/componets/galleryItem/galleryItem.jsx
+++ src/componets/galleryItem/galleryItem.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import "./galleryItem.css";
 
 const galleryItem = ({ item }) => {
   return (
-    <div className="galleryItem">
+    <div className="galleryItem" style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}>
       <img src={item.media} alt="" />
     </div>
   );
