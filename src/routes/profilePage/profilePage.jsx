b92d6ba46b9219fa0119d5f841e5919e3476ff88
punya <punya8144@gmail.com>
2025-04-17 20:40:00 +0530

    user profile page design

--- src/routes/profilePage/profilePage.jsx
+++ src/routes/profilePage/profilePage.jsx
@@ -1,7 +1,34 @@
 import "./profilePage.css";
+import Image from "../../componets/image/image";
+import { useState } from "react";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const [type, setType] = useState("saved");
+
+  return (
+    <div className="profilePage">
+      <Image className="profileImage" path="/general/noAvatar.png" w={100} h={100} alt="" />
+      <h1 className="profileName">Punya</h1>
+      <span className="profileUsername">@Punya</span>
+      <div className="profileCounts">10 followers . 20 followings</div>
+      <div className="profileInteractions">
+        <Image path="/general/share.svg" alt="" />
+        <div className="profileButtons">
+          <button>Message</button>
+          <button>Follow</button>
+        </div>
+        <Image path="/general/more.svg" />
+      </div>
+      <div className="profileOptions">
+        <span onClick={() => setType("created")} className={type === "created" ? "active" : ""}>
+          Created
+        </span>
+        <span onClick={() => setType("saved")} className={type === "saved" ? "active" : ""}>
+          Saved
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
